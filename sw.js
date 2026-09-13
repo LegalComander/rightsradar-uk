ce6cbfa9719fd91d0f967372cdbe21575503df44
LegalComander <ukrightsradar@gmail.com>
2026-09-13 19:45:17 +0100

    Refresh PWA cache for simplified reading mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'rightsradar-pwa-v6';
+const CACHE_VERSION = 'rightsradar-pwa-v7';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const SAVED_CACHE = `${CACHE_VERSION}-saved`;

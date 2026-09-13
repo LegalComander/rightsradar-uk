36fc71ecec57082017b65564c43beba015b136cb
LegalComander <ukrightsradar@gmail.com>
2026-09-13 07:14:40 +0100

    Fix Android 16 predictive back lint check

--- android-app/app/src/main/java/uk/rightsradar/app/MainActivity.java
+++ android-app/app/src/main/java/uk/rightsradar/app/MainActivity.java
@@ -166,6 +166,7 @@ public class MainActivity extends Activity {
         }
     }
 
+    @SuppressLint("GestureBackNavigation")
     @SuppressWarnings("deprecation")
     @Override
     public void onBackPressed() {
